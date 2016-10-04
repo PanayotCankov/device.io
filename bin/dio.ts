c27ae67d614343155a084f14a14df422de2ed92b
Panayot Cankov <Panayot.Cankov@telerik.com>
2016-10-04 16:32:15 +0300

    Print the version in JSON for consistency

--- bin/dio.ts
+++ bin/dio.ts
@@ -7,7 +7,7 @@ let child_process = require("child_process");
 let readline = require("readline");
 
 let config = JSON.parse(fs.readFileSync(path.join(__dirname, "../package.json")));
-console.log(`device.io ${config.version}`);
+console.log(JSON.stringify({"device.io": config.version}));
 
 let idb = child_process.spawn("bin/idb");
 let idbline = readline.createInterface({ input: idb.stdout });
