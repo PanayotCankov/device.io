0bd8a4492f311465477420a81a98577221604bcd
Panayot Cankov <Panayot.Cankov@telerik.com>
2016-10-04 16:27:32 +0300

    Add dummy JS consumer for the idb

--- bin/dio.ts
+++ bin/dio.ts
@@ -3,6 +3,12 @@
 declare var console, require, __dirname;
 let fs = require("fs");
 let path = require("path");
+let child_process = require("child_process");
+let readline = require("readline");
 
 let config = JSON.parse(fs.readFileSync(path.join(__dirname, "../package.json")));
 console.log(`device.io ${config.version}`);
+
+let idb = child_process.spawn("bin/idb");
+let idbline = readline.createInterface({ input: idb.stdout });
+idbline.on("line", line => console.log(line));
